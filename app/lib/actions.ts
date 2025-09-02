110c1fdd2ba52b025bc20b8bbae6bdf9d6dd8e2f
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-09-02 13:27:17 +0200

    Refactor error handling and improve ESLint configuration; update dependencies

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,9 +4,9 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 
-import { redirect } from "next/navigation";
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
+import { redirect } from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -55,6 +55,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error);
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -88,6 +89,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
   } catch (error) {
+    console.error(error);
     return { message: "Database Error: Failed to Update Invoice." };
   }
   revalidatePath("/dashboard/invoices");
@@ -112,7 +114,7 @@ export async function authenticate(
     await signIn("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      switch ((error as any).type) {
+      switch (error.constructor.name) {
         case "CredentialsSignin":
           return "Invalid credentials.";
         default:
